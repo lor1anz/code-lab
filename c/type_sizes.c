1ebd93c9ea093ada8ebe3a85aa19299144d0a1c1
lorianz <mrkraft500@gmail.com>
2026-01-04 15:27:44 +0300

    C: malloc check

--- c/type_sizes.c
+++ c/type_sizes.c
@@ -1,19 +1,20 @@
 #include <limits.h>
 #include <float.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+void check_malloc(unsigned nbytes);
 
 int main(void)
 {
     printf("------Void-------\n");
-
     printf("\n");
 
     printf("void %ld\n", sizeof(void));
 
     printf("\n\n");
-
     printf("------Integer-------\n");
-
     printf("\n");
 
     printf("char %ld\n", sizeof(char));
@@ -44,9 +45,7 @@ int main(void)
     printf("signed long long max %lld\n", ULLONG_MAX);
 
     printf("\n\n");
-
     printf("------Float------\n");
-
     printf("\n");
 
     printf("float %ld\n", sizeof(float));
@@ -59,5 +58,27 @@ int main(void)
     printf("double dig %d\n", DBL_DIG);
     printf("long double dig %d\n", LDBL_DIG);
 
+    printf("\n\n");
+    printf("----Malloc----\n");
+    printf("\n");
+    
+    check_malloc(1);
+    check_malloc(3);
+    check_malloc(4);
+    check_malloc(7);
+    check_malloc(8);
+    check_malloc(11);
+    check_malloc(32);
+    check_malloc(50);
+
+    printf("\n");
+
     return 0;
+}
+
+void check_malloc(unsigned nbytes)
+{
+    void *mem = malloc(nbytes * sizeof(char));
+    printf("bytes requested %2u: %lu\n", nbytes, malloc_usable_size(mem));
+    free(mem);
 }
